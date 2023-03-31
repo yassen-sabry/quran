96df66e235e97b1ac87b4d2ccd8450d295959297
yassen-sabry <yassen006999@gmail.com>
2023-03-31 21:25:31 +0300

    part 4

---
+++ lib/arabic surah number.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:quran/to_arabic_number.dart';
+
+class ArabicSurahNumber extends StatelessWidget {
+  const ArabicSurahNumber({Key? key,required this.i}) : super(key: key);
+    final int i;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('\uFD3E'+(i+1).toString().toArabicNumbers+"\uFD3F",style: const TextStyle(
+      color: Color.fromARGB(255, 0, 0, 0),
+      fontFamily: 'me_quran',
+      fontSize: 20,
+      shadows: [
+        Shadow(
+          offset: Offset(.5, .5),
+          blurRadius: 1.0,
+          color: Colors.amberAccent,
+        )
+      ]
+    ),);
+
+  }
+}
